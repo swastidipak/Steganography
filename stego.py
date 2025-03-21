9e8a396fda92801bee16c637756e3a7a79a0e646
Swasti Dipak Biswas <swastidipak@gmail.com>
2025-03-21 15:35:14 +0530

    Update stego.py

--- stego.py
+++ stego.py
@@ -2,7 +2,7 @@ import cv2
 import os
 import string
 
-img = cv2.imread("mypic.jpg") # Replace with the correct image path#image
+img = cv2.imread("sample_image.jpg") # Replace with the correct image path#image
 
 msg = input("Enter secret message:")
 password = input("Enter a passcode:")
